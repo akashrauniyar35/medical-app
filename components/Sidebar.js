18d0e7b268caea914590cc5031d52183dbbe79bd
Akash Rouniyar <akashrouniyar@192-168-1-106.tpgi.com.au>
2021-08-19 17:18:50 +1000

    next js and tailwind setup

---
+++ components/Sidebar.js
@@ -0,0 +1,20 @@
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import Link from 'next/link'
+
+
+function Sidebar() {
+    return (
+        <div className="absolute top-0 bg-purple-400 w-20 rounded-xl flex flex-col items-center gap-4 p-3 h-screen cursor-pointer">
+            <Link href="/dashboard">
+            <div className="animate-spin"> <DashboardIcon fontSize="large"/></div>
+            </Link>
+            <div> <DashboardIcon fontSize="large"/></div>
+            <div> <LocalHospitalIcon fontSize="large"/></div>
+            <div> <DashboardIcon fontSize="large"/></div>
+           
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+
+function dashboard() {
+
+    return (
+        <div className="bg-gray-200 m-20"> 
+            <h1 className="text-2xl font-semibold">Dashboard</h1>
+            <button className="btn text-red-500 lg:text-5xl">Click me</button>
+        </div>
+    )
+}
+
+export default dashboard
